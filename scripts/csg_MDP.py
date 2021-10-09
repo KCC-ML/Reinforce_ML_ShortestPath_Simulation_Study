922731146d997e1b9f7cae12dde012b1b8d717a5
SG <80019221+Machinity@users.noreply.github.com>
2021-10-09 19:27:14 +0900

    policy improvement

--- scripts/csg_MDP.py
+++ scripts/csg_MDP.py
@@ -1,20 +1,22 @@
 import numpy as np
-from packman_control import World
+from packman_control_5by5 import World
 
 class MDP:
     def __init__(self, canvas_grid):
         self.grid_dim = canvas_grid.grid_dim
-        self.State_no = (self.grid_dim ** 2) * 4
+        self.State_sum = (self.grid_dim ** 2) * 4
         self.agent = canvas_grid.pacman
-        self.walls = self.agent.wall_lines()
+        self.walls = canvas_grid.wall_lines()
         self.reward = -1
         self.gamma = 0.9
         self.action = [0, 1, 2]
         self.theta = 0.00001
-        self.R = np.ones((self.State_no, 1)) * -1
+        self.R = np.ones((self.State_sum, 1)) * -1
         self.initial_policy = [0.8, 0.1, 0.1]
-        self.initial_P = np.zeros((self.State_no, self.State_no))
-        self.initial_V = np.zeros((self.State_no, 1))
+        self.initial_P = np.zeros((self.State_sum, self.State_sum))
+        self.initial_V = np.zeros((self.State_sum, 1))
+        self.iteration = 1
+        self.target_value = 0
 
     # https://www.youtube.com/watch?v=3vuw23_l_oA&list=PLKs7xpqpX1beJ5-EOFDXTVckBQFFyTxUH&index=6 28:29
     # 위 링크 강의 영상에서 아래식을 가지고 코딩
@@ -22,36 +24,36 @@ class MDP:
 
     # P파이 를 구하기 위한 메소드(P = 100x100의 s -> s'를 표현한 행렬)
     # policy_improvement 가 끝난 후에 다시 구해줘야 함
-    def state_transition_matrix(self, post_P, improved_policy):
+    def state_transition_matrix(self, post_P, State_no, improved_policy):
         P = post_P
-        for i in range(self.State_no):
-            direction = i % 4
-            row = (i // 4) // self.grid_dim
-            col = (i // 4) % self.grid_dim
+        direction = State_no % 4
+        row = (State_no // 4) // self.grid_dim
+        col = (State_no // 4) % self.grid_dim
 
-            if self.walls[direction, row, col] == 1:
-                P[i, i - direction + ((direction - 1) % 4)] = improved_policy[1]
-                P[i, i - direction + ((direction + 1) % 4)] = improved_policy[2]
-                P[i, i] = improved_policy[0]
+        if self.walls[direction, row, col] == 1:
+            P[State_no, State_no - direction + ((direction - 1) % 4)] = improved_policy[1]
+            P[State_no, State_no - direction + ((direction + 1) % 4)] = improved_policy[2]
+            P[State_no, State_no] = improved_policy[0]
 
-            elif self.walls[direction, row, col] == 0:
-                P[i, i - direction + ((direction - 1) % 4)] = improved_policy[1]
-                P[i, i - direction + ((direction + 1) % 4)] = improved_policy[2]
-                if direction % 4 == 0:
-                    tmp = -4 * self.grid_dim
-                elif direction % 4 == 1:
-                    tmp = 4 * 1
-                elif direction % 4 == 2:
-                    tmp = 4 * self.grid_dim
-                elif direction % 4 == 3:
-                    tmp = -4 * 1
-                P[i, i + tmp] = improved_policy[0]
+        elif self.walls[direction, row, col] == 0:
+            P[State_no, State_no - direction + ((direction - 1) % 4)] = improved_policy[1]
+            P[State_no, State_no - direction + ((direction + 1) % 4)] = improved_policy[2]
+            if direction % 4 == 0:
+                tmp = -4 * self.grid_dim
+            elif direction % 4 == 1:
+                tmp = 4 * 1
+            elif direction % 4 == 2:
+                tmp = 4 * self.grid_dim
+            elif direction % 4 == 3:
+                tmp = -4 * 1
+            P[State_no, State_no + tmp] = improved_policy[0]
 
-        if not np.all((P.sum(axis=1)) == 1):
-            print("check transition probability matrix!!")
+        # if not np.all((P.sum(axis=1)) == 1):
+        #     print("check transition probability matrix!!")
+
+        # self.P = P
+        # np.set_printoptions(threshold=np.inf, linewidth=np.inf)
         # print("transition probability matrix: \n", P)
-        self.P = P
-
         return P
 
     # vk(=v_post)를 구하기 위한 메소드(vk는 각 v(s)를 100x1의 벡터로 표현한 행렬)
@@ -59,29 +61,89 @@ class MDP:
     # , R파이(R파이(=R) 는 R(=-1)을 100x1의 벡터로 표현한 행렬)를 더한다)
     def policy_evaluation(self, R, P, post_V):
         V = R + self.gamma * P @ post_V
+        for i in range(45, 49):
+            V[i, 0] = self.target_value
+        np.set_printoptions(threshold=np.inf, linewidth=np.inf)
+        print(V)
+        print("-----------------------------------------------------------")
         return V
 
     # 위에 구해진 vk+1로 agent 가 각 v(s)를 비교하여 가장 높은 v(s)를 택하는 action 을 취할 수 있도록 policy(0.8, 0.1, 0.1)를
     # improve 한다
-    def policy_improvement(self, next_V, policy):
-        for i in range(100):
-            next_V
+    def policy_improvement(self, next_V, State_no):
+        direction = State_no % 4
+        row = (State_no // 4) // self.grid_dim
+        col = (State_no // 4) % self.grid_dim
+        if self.walls[direction, row, col] == 1:
+            v_left = next_V[State_no - direction + ((direction - 1) % 4), 0]
+            v_right = next_V[State_no - direction + ((direction + 1) % 4), 0]
+            v_straight = next_V[State_no, 0]
+
+            if v_straight == v_left and v_straight == v_right:
+                improved_policy = [0.5, 0.5, 0.5]
+            elif v_straight > v_left and v_straight > v_right:
+                improved_policy = [1, 0, 0]
+            elif v_left > v_straight and v_left > v_right:
+                improved_policy = [0, 1, 0]
+            elif v_right > v_straight and v_right > v_left:
+                improved_policy = [0, 0, 1]
+            elif v_straight == v_left and v_left > v_right:
+                improved_policy = [0.5, 0.5, 0]
+            elif v_straight == v_right and v_right > v_left:
+                improved_policy = [0.5, 0, 0.5]
+            elif v_left == v_right and v_left > v_straight:
+                improved_policy = [0, 0.5, 0.5]
+
+        elif self.walls[direction, row, col] == 0:
+            v_left = next_V[State_no - direction + ((direction - 1) % 4), 0],
+            v_right = next_V[State_no - direction + ((direction + 1) % 4), 0],
+            if direction % 4 == 0:
+                tmp = -4 * self.grid_dim
+            elif direction % 4 == 1:
+                tmp = 4 * 1
+            elif direction % 4 == 2:
+                tmp = 4 * self.grid_dim
+            elif direction % 4 == 3:
+                tmp = -4 * 1
+            v_straight = next_V[State_no + tmp, 0]
+
+            if v_straight == v_left and v_straight == v_right:
+                improved_policy = [0.5, 0.5, 0.5]
+            elif v_straight > v_left and v_straight > v_right:
+                improved_policy = [1, 0, 0]
+            elif v_left > v_straight and v_left > v_right:
+                improved_policy = [0, 1, 0]
+            elif v_right > v_straight and v_right > v_left:
+                improved_policy = [0, 0, 1]
+            elif v_straight == v_left and v_left > v_right:
+                improved_policy = [0.5, 0.5, 0]
+            elif v_straight == v_right and v_right > v_left:
+                improved_policy = [0.5, 0, 0.5]
+            elif v_left == v_right and v_left > v_straight:
+                improved_policy = [0, 0.5, 0.5]
+
+        return improved_policy
 
     # 위 과정을 반복
     def policy_iteration(self):
-        iteration = 1
-        next_P = self.state_transition_matrix(self.initial_P, self.initial_policy)
+        print("iteration: ", self.iteration)
+        for i in range(self.State_sum):
+            next_P = self.state_transition_matrix(self.initial_P, i, self.initial_policy)
         next_V = self.policy_evaluation(self.R, next_P, self.initial_V)
-        improved_policy = self.policy_improvement(next_V, self.initial_policy)
-        while True:
-            iteration += 1
-            post_V = next_V
-            next_P = self.state_transition_matrix(next_P, improved_policy)
-            next_V = self.policy_evaluation(self.R, next_P, next_V)
-            improved_policy = self.policy_improvement(next_V, improved_policy)
+        for i in range(self.State_sum):
+            next_P = self.state_transition_matrix(next_P, i, self.policy_improvement(next_V, i))
 
-            if self.theta > min(next_V - post_V):
-                optimal_policy = improved_policy
+        while True:
+            self.iteration += 1
+            print("iteration: ", self.iteration)
+            post_V = next_V
+            next_V = self.policy_evaluation(self.R, next_P, post_V)
+            for i in range(self.State_sum):
+                next_P = self.state_transition_matrix(next_P, i, self.policy_improvement(next_V, i))
+
+            if self.theta > abs(min(next_V - post_V)):
+                optimal_policy = next_P
+                print(next_V)
                 break
 
         return optimal_policy
