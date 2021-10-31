5912a412860d4b862114edea29d981d0f260a27e
cshiz9206 <64184518+cshiz9206@users.noreply.github.com>
2021-10-31 12:13:26 +0900

    <Feat> First-visit MC Control On-policy - MC_Control.py     + algorithm coding done     + test(5 by 5, start fixed, end fixed, wall fixed) done

--- scripts/MC_Control.py
+++ scripts/MC_Control.py
@@ -11,9 +11,9 @@ class MCControl():
         self.gamma = 0.9
         self.state_num = self.world.grid_dim ** 2 * self.agent_direction_count
         self.target_position = self.world.env.gridmap_goal
-        self.start_policy = [0.8, 0.1, 0.1]
         self.actions = self.translate_action_index(self.world.pacman_action_list)
-        self.A = []
+        self.epsilon = 0.1
+        self.start_policy = [1 / len(self.actions), 1 / len(self.actions), 1 / len(self.actions)]
         self.greedy_A = []
         self.initialize_data()
 
@@ -21,13 +21,12 @@ class MCControl():
         # T : episode 종료 시점 : int
         # pairs : t를 index로 갖고, pair<state, action> index를 element로 갖는 pair<state, action>의 집합 : vector(inf by 1)
         # pair : <state, action> : list(len : 4)
-        # N : state counter, episode 중 나타났던 state 별로 횟수를 센다 : vector(state 수 by 1)
-        # Gs : returns, 각 episode에서의 각 state에 대한 G 집합 : vector(state 수 by 1)
-        # R : reward 집합, target index 외 모두 -1 : vector(state 수 by 1)
+        # N : pair counter, episode 중 나타났던 pair 별로 횟수를 센다 : vector(pair 수 by 1)
+        # Gs : returns, 각 episode에서의 각 pair에 대한 G 집합 : vector(pair 수 by 1)
+        # R : reward 집합, target index 외 모두 -1 : vector(pair 수 by 1)
         # Q : 각 pair<state, action>에 대한 value 집합 : vector(state 수 * action 수 by 1)
-        # policy : matrix[state 수][action 수]
-        # A : probability of actions : matrix[state 수][action 수]
-        # greedy_A : max probability actions among the actions(arg max_a Q(s,a)) : matrix[state 수][action 수]
+        # policy : matrix[state 수][action 수] (start_policy : epsilon-soft policy)
+        # greedy_A : max probability actions among the actions(arg max_a Q(s,a)) : vector(state 수 by range(action 수))
         # 0으로 초기화 : N, Gs, Q
 
     def translate_action_index(self, actions):
@@ -37,7 +36,6 @@ class MCControl():
         return pacman_action_index
 
     def initialize_data(self):
-        # self.initialize_pairs()
         self.initialize_Q()
         self.initialize_N()
         self.initialize_R()
@@ -45,19 +43,20 @@ class MCControl():
         self.matrixization_policy()
 
     def initialize_Q(self):
-        self.Q = np.zeros((self.state_num * self.agent_direction_count, 1))
+        self.Q = np.zeros((self.state_num * len(self.actions), 1))
 
     def initialize_N(self):
-        self.N = np.zeros((self.state_num, 1))
+        self.N = np.zeros((self.state_num * len(self.actions), 1))
 
     def initialize_R(self):
-        self.R = self.reward * np.ones((self.state_num, 1))
-        temp = self.agent_direction_count * self.world.grid_dim * self.target_position[0] + self.agent_direction_count * \
-               self.target_position[1]
-        self.R[temp: temp + self.agent_direction_count] = 0
+        self.R = self.reward * np.ones((self.state_num * len(self.actions), 1))
+        temp = self.agent_direction_count * self.world.grid_dim * len(self.actions) * self.target_position[0] + self.agent_direction_count * len(self.actions) * self.target_position[1]
+        self.R[temp: temp + self.agent_direction_count * len(self.actions)] = 0
+        print(self.R.size)
+        print(np.where(self.R == 0))
 
     def initialize_Gs(self):
-        self.Gs = np.zeros((self.state_num, 1))
+        self.Gs = np.zeros((self.state_num * len(self.actions), 1))
 
     def matrixization_policy(self):
         self.policy_matrix = np.reshape(self.start_policy * self.state_num, (self.state_num, len(self.start_policy)))
@@ -66,27 +65,29 @@ class MCControl():
 
     def iteration(self):
         episode = 1
-        while episode < 200:
+        while episode < 3000:
+            print('\nepisode = ', episode)
             T, pairs = self.create_episode()   # episode 완료 -> T, pairs 확정
             G = 0
             for t in range(T-1, -1, -1):
-                s_index = self.transform_state_index(pairs[t])
-                G = self.gamma * G + self.R[s_index]
-                if pairs[t] not in pairs[:t]:
-                    self.Gs[s_index] += G
-                    self.N[s_index] += 1
-                    #print(self.Gs)
+                pair_index = self.transform_pair_index(pairs[t])
+                G = self.gamma * G + self.R[pair_index]
+                if pairs[t].tolist() not in pairs[:t].tolist():
+                    self.Gs[pair_index] += G
+                    self.N[pair_index] += 1
+                    self.Q[pair_index] = self.Gs[pair_index] / self.N[pair_index]
+            for s_index in range(self.state_num):
+                s_action_values = self.Q[s_index * len(self.actions): s_index * len(self.actions) + 3]
+                tmp = np.squeeze(s_action_values)
+                self.greedy_A.append([])
+                self.greedy_A[s_index] = np.where(tmp == np.max(tmp))
+                for action_index in range(len(self.actions)):
+                    self.policy_matrix[s_index][action_index] = self.epsilon / len(self.actions)
+                    if np.any(action_index == self.greedy_A[s_index][0]):
+                        self.policy_matrix[s_index][action_index] = (1 - self.epsilon + self.greedy_A[s_index][0].size * self.epsilon / len(self.actions)) / self.greedy_A[s_index][0].size
             episode += 1
-            print(episode)
-        for s in pairs:
-            np.seterr(invalid='ignore')
-            s_index = self.transform_state_index(s)
-            self.Q[s_index] = self.Gs[s_index] / self.N[s_index]
-
-        print(self.Q.reshape(-1, 4))
 
         time.sleep(5)
-        self.policy_improvement()
         self.world.iter_step()
 
         return self.Q
@@ -94,55 +95,41 @@ class MCControl():
 
     def create_episode(self):
         T, pairs = self.world.iter_step()
-        #print(T, pairs)
         return T, pairs
 
-    def transform_state_index(self, s):
-        return s[0] * self.world.grid_dim * self.agent_direction_count + s[1] * self.agent_direction_count + s[2]
+    def transform_pair_index(self, pair):
+        unit_row = self.agent_direction_count * self.world.grid_dim * len(self.actions)
+        unit_col = self.agent_direction_count * len(self.actions)
+        return pair[0][0] * unit_row + pair[0][1] * unit_col + pair[0][2] * len(self.actions) + pair[0][3]
+
+    # def transform_index_pair(self, pair_index):
+    #     unit_row = self.agent_direction_count * self.world.grid_dim * len(self.actions)
+    #     unit_col = self.agent_direction_count * len(self.actions)
+    #     row = pair_index // unit_row
+    #     col = pair_index % unit_row // unit_col
+    #     direction = pair_index % unit_row % unit_col // len(self.actions)
+    #     action = pair_index % unit_row % unit_col % len(self.actions)
+    #     return [row, col, direction, action]
 
 
-    def policy_improvement(self):
-        for now_index in range(self.state_num):
-            state_present = self.transform_index_state(now_index)
-            if state_present[0:2] != list(self.target_position):
-                self.policy_greedy_update(now_index, state_present)
-                # print('policy_matrix : \n{}\n'.format(self.policy_matrix))
-        print(self.policy_matrix.reshape(-1, 3))
-
-    def policy_greedy_update(self, now_index, state_present):
-        next_values = []
-        for action in self.actions:
-            next_index = self.next_state(state_present, action)
-            next_values.append(self.Q[next_index])
-        greedy_action = [i for i in range(len(next_values)) if next_values[i] == max(next_values)]
-        self.policy_matrix[now_index, :] = 0
-        self.policy_matrix[now_index, greedy_action] = 1 / len(greedy_action)
-
-    def transform_index_state(self, state_index):
-        row = state_index // (self.agent_direction_count * self.world.grid_dim)
-        col = state_index % (self.agent_direction_count * self.world.grid_dim) // self.agent_direction_count
-        direction = state_index % (self.agent_direction_count * self.world.grid_dim) % self.agent_direction_count
-        return [row, col, direction]
-
-
-    def next_state(self, state_present, action):
-        state_next = copy.deepcopy(state_present)
+    def next_pair(self, pair_present, action):
+        pair_next = copy.deepcopy(pair_present)
         if action == 0:
-            if self.world.env.walls[state_present[2]][state_present[0]][state_present[1]] == 1:
+            if self.world.env.walls[pair_present[2]][pair_present[0]][pair_present[1]] == 1:
                 pass
             else:
-                if state_present[2] == 0:
-                    state_next[0] -= 1
-                elif state_present[2] == 1:
-                    state_next[1] += 1
-                elif state_present[2] == 2:
-                    state_next[0] += 1
-                elif state_present[2] == 3:
-                    state_next[1] -= 1
+                if pair_present[2] == 0:
+                    pair_next[0] -= 1
+                elif pair_present[2] == 1:
+                    pair_next[1] += 1
+                elif pair_present[2] == 2:
+                    pair_next[0] += 1
+                elif pair_present[2] == 3:
+                    pair_next[1] -= 1
         elif action == 1:
-            state_next[2] = (state_next[2] - 1) % self.agent_direction_count
+            pair_next[2] = (pair_next[2] - 1) % self.agent_direction_count
         elif action == 2:
-            state_next[2] = (state_next[2] + 1) % self.agent_direction_count
+            pair_next[2] = (pair_next[2] + 1) % self.agent_direction_count
 
-        next_index = state_next[0] * (self.agent_direction_count * self.world.grid_dim) + state_next[1] * self.agent_direction_count + state_next[2]
+        next_index = pair_next[0] * (self.agent_direction_count * self.world.grid_dim) + pair_next[1] * self.agent_direction_count + pair_next[2]
         return next_index
--- scripts/pacman_control.py
+++ scripts/pacman_control.py
@@ -9,10 +9,8 @@ class World:
         self.grid_dim = n;
         self.env = Env(n)
         self.pacman = Pacman(n)
-        gridmap = self.pacman.gridmap
         print("Initialize")
         self.pacman.visualization()
-
 
         print("-------------------------------")
         input("press any key")
@@ -20,7 +18,6 @@ class World:
         self.step = 0
         self.pacman_action_list = ["straight", "left", "right"]
         self.pacman_cardinal_points = ["north", "east", "south", "west"]
-
 
         self.model = MCControl(self)
         self.thread = threading.Thread(target=self.model.iteration)
@@ -38,12 +35,15 @@ class World:
 
         self.window.mainloop()
 
+        print(self.model.Q.reshape(-1, 3))
+        print(self.model.policy_matrix)
         print("Pacman arrived at goal in {} steps.".format(self.step))
 
 
     def iter_step(self):
         T = 0
         pairs = []
+        self.step = 0
         self.pacman.position = self.pacman.first_position
         while True:
             # if event.keysym and np.any(self.pacman.gridmap_goal != self.pacman.position):
@@ -57,8 +57,8 @@ class World:
             pacman_state = np.append(self.pacman.position, self.pacman_cardinal_points.index(self.pacman.cardinal_point))
             if pacman_direction == "straight":
                 if self.pacman.straight(self.env.walls) == 1:
-                    return T, pairs
-                    #self.window.destroy()
+                    print('step = ', self.step)
+                    return T, np.array(pairs)
                 #self.pacman.visualization()
             elif pacman_direction == "left":
                 self.pacman.left()
@@ -67,12 +67,15 @@ class World:
                 self.pacman.right()
                 #self.pacman.visualization()
             self.cv.set_agent(self.pacman, self.pacman.cardinal_point)
-            #time.sleep(0.5)
+            time.sleep(0.01)
+
+            pairs.append([])
+            tmp = pacman_state.tolist()
+            tmp.append(self.pacman_action_list.index(pacman_direction))
+            pairs[T].append(tmp)
+            #print(tmp)
 
             T += 1
-            pairs.append(pacman_state.tolist().append(pacman_direction))
-            #print(pairs, T)
-
 
 if __name__ == '__main__':
     World(5).main()
