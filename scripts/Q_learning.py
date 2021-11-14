a8b023fec19199529cca77c7ff182dd5787285f4
cshiz9206 <64184518+cshiz9206@users.noreply.github.com>
2021-11-14 09:41:55 +0900

    <Feat> Q-learning - Q_learning.py     + succeeded in finding optimal policy - pacman_control.py     + modified for Q-learning - SARSA....py     + deleted files related to SARSA

--- scripts/Q_learning.py
+++ scripts/Q_learning.py
@@ -2,9 +2,8 @@ import numpy as np
 import time
 import math
 import copy
-from collections import deque
 
-class SARSALF:
+class QLearning:
     def __init__(self, world):
         self.world = world
         self.agent_direction_count = self.world.env.walls.shape[0]
@@ -14,8 +13,7 @@ class SARSALF:
         self.target_position = self.world.env.gridmap_goal
         self.actions = self.translate_action_index(self.world.pacman_action_list)
         self.epsilon = 0.1
-        self.alpha = 0.5
-        self.lbda = 0.9
+        self.alpha = 0.01
         self.start_policy = [1 / len(self.actions), 1 / len(self.actions), 1 / len(self.actions)]
         self.greedy_A = []
         self.initialize_data()
@@ -29,33 +27,30 @@ class SARSALF:
 
     def initialize_data(self):
         self.initialize_Q()
-        # self.initialize_R()
+        self.initialize_R()
         self.matrixization_policy()
 
     def initialize_Q(self):
         self.Q = np.zeros((self.state_num * len(self.actions), 1))
 
     def initialize_R(self):
-        self.R = self.reward * np.zeros((self.state_num * len(self.actions), 1))
-        # temp = self.agent_direction_count * self.world.grid_dim * len(self.actions) * self.target_position[0] + self.agent_direction_count * len(self.actions) * self.target_position[1]
-        # self.R[temp: temp + self.agent_direction_count * len(self.actions)] = 5
-        # print(self.R.size)
-        # print(np.where(self.R == 0))
+        self.R = self.reward * np.ones((self.state_num * len(self.actions), 1))
+        temp = self.agent_direction_count * self.world.grid_dim * len(self.actions) * self.target_position[0] + self.agent_direction_count * len(self.actions) * self.target_position[1]
+        self.R[temp: temp + self.agent_direction_count * len(self.actions)] = 5
 
     def matrixization_policy(self):
         self.policy_matrix = np.reshape(self.start_policy * self.state_num, (self.state_num, len(self.start_policy)))
-        # temp = self.agent_direction_count * self.world.grid_dim * self.target_position[0] + self.agent_direction_count * self.target_position[1]
-        # #self.policy_matrix[temp : temp + self.agent_direction_count, :] = 10
+        temp = self.agent_direction_count * self.world.grid_dim * self.target_position[0] + self.agent_direction_count * self.target_position[1]
+        self.policy_matrix[temp : temp + self.agent_direction_count, :] = 10
 
     def iteration(self):
         self.steps = []
         episode = 0
         past_episode_step = math.inf
-        while episode < 1000:
+        while episode < 5000:
             episode += 1
             print('\nepisode = ', episode)
             step = self.policy_evaluation()
-            self.policy_improvement()
 
             self.steps.append(step)
             if step < past_episode_step:
@@ -77,45 +72,28 @@ class SARSALF:
         pacman_direction_index = self.world.pacman_action_list.index(pacman_direction)
         pair = np.append(self.world.pacman.position, cardinal_point_index)
         pair = np.append(pair, pacman_direction_index)
-        t = 1
-        T = math.inf
-        pairs = deque()
-        pairs.append(pair)
-        R = deque()
-        R.append(-1)
+        step = 0
         while True:
-            if t < T:
-                next_pair, reward = self.world.iter_step(pacman_direction)
-                pacman_direction = np.random.choice(self.world.pacman_action_list, 1, p=self.policy_matrix[
+            step += 1
+            next_s, R = self.world.iter_step(pacman_direction)
+            pacman_direction = np.random.choice(self.world.pacman_action_list, 1, p=self.policy_matrix[
                 self.world.pacman.position[0] * (4 * 5) + self.world.pacman.position[
                     1] * 4 + self.world.pacman_cardinal_points.index(self.world.pacman.cardinal_point)])
-                pacman_direction_index = self.world.pacman_action_list.index(pacman_direction)
-                next_pair = np.append(next_pair, pacman_direction_index)
-                pairs.append(next_pair)
-                R.append(reward)
-                if np.all(next_pair[:2] == self.target_position):
-                    T = t + 1
-                    break
-                t += 1
-        print('total_step = ', t)
+            pacman_direction_index = self.world.pacman_action_list.index(pacman_direction)
+            next_pair = np.append(next_s, pacman_direction_index)
 
-        for t in range(T):
-            lambda_G = 0
-            for n in range(1, T):
-                nstep_G = 0
-                for i in range(1, n + 1):
-                    nstep_G += (self.gamma ** (i - 1)) * R[i]
-                lambda_G += (self.lbda ** n) * (
-                            nstep_G + (self.gamma ** n) * self.Q[self.transform_pair_index(pairs[n])])
-            lambda_G = (1 - self.lbda) * lambda_G + (self.lbda ** (T - 1)) #* nstep_G
-            self.Q[self.transform_pair_index(pairs[0])] += self.alpha * (
-                        lambda_G - self.Q[self.transform_pair_index(pairs[0])])
+            pair_index = self.transform_pair_index(pair)
+            #next_pair_index = self.transform_pair_index(next_pair)
+            next_state_index = self.transform_s_index(next_s)
+            self.Q[pair_index] += self.alpha * (R + self.gamma * np.max(self.Q[next_state_index : next_state_index + len(self.actions)]) - self.Q[pair_index])
+            if (np.all(self.target_position == next_pair[:2])):
+                break
 
-            pairs.popleft()
-            R.popleft()
-            T -= 1
+            self.policy_improvement()  # for on-line
 
-        return t
+            pair = next_pair
+        print('total_step = ', step)
+        return step
 
 
     def policy_improvement(self):
@@ -138,6 +116,11 @@ class SARSALF:
         unit_row = self.agent_direction_count * self.world.grid_dim * len(self.actions)
         unit_col = self.agent_direction_count * len(self.actions)
         return pair[0] * unit_row + pair[1] * unit_col + pair[2] * len(self.actions) + pair[3]
+
+    def transform_s_index(self, state):
+        unit_row = self.agent_direction_count * self.world.grid_dim * len(self.actions)
+        unit_col = self.agent_direction_count * len(self.actions)
+        return state[0] * unit_row + state[1] * unit_col + state[2] * len(self.actions) + 0
 
     # def transform_index_pair(self, pair_index):
     #     unit_row = self.agent_direction_count * self.world.grid_dim * len(self.actions)
